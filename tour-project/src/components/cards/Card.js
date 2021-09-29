6010dd5093bf378e1c1729c31461f325a860a3fc
argun8394 <argundoganali@gmail.com>
2021-09-29 21:33:24 +0300

    card

--- tour-project/src/components/cards/Card.js
+++ tour-project/src/components/cards/Card.js
@@ -2,16 +2,66 @@ import React from 'react';
 import {data} from "../../helpers/Data";
 import "./Card.css";
 
+
 function Card(){
     return (
         <div className="card-container">
-            <div className="cards">
-                <h2>{data[0].title}</h2>
-            </div>
-            <img src={data[0].image} alt={data[0].title}/>
-            
+            {data.map((card)=>{
+return (
+
+    <div className="cards">
+        <div className="title">
+            <h2>{card.title}</h2>
+        </div>
+    </div>
+)
+            })}
+
+
         </div>
     );
 
 };
+
 export default Card;
+
+
+
+
+{/* 
+// tektek döndürme işlemi
+function Card(){
+    return (
+        <div className="card-container">
+            <div className="cards">
+                <div className="title">
+                    <h2>{data[0].title}</h2>
+                </div>
+
+                <img src={data[0].image} alt={data[0].title}/>
+
+                <div className="card-over">
+                    <p>{data[0].desc}</p>
+                </div>
+            </div>
+            <div className="cards">
+                <div className="title">
+                    <h2>{data[1].title}</h2>
+                </div>
+
+                <img src={data[1].image} alt={data[1].title}/>
+
+                <div className="card-over">
+                    <p>{data[1].desc}</p>
+                </div>
+            </div>
+            .
+            .
+            .
+            .
+
+        </div>
+    );
+
+};
+*/}
